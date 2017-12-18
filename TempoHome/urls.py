b45c588965d73c10c40dab2d830692d95a6df010
lauricdd <lauri_cdd@hotmail.com>
2017-12-18 12:07:42 +0100

    Project settings

--- TempoHome/urls.py
+++ TempoHome/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('usersprofiles.urls', namespace='usersprofiles')),
+    url(r'^', include('profiles.urls', namespace='profiles')),
 ]

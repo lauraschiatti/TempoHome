977952f65485da4a7152cee63aaa6517458e7bb0
lauricdd <lauri_cdd@hotmail.com>
2017-12-21 22:22:18 +0100

    Sign up method on views and url

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 from . import views
+from .views import *
 
 urlpatterns = [
-    url(r'^', views.home, name="home"),
-    url(r'^login/$', views.authentication, name="authentication"),
+    url(r'^$', views.home, name="home"),
+    url(r'^signup', views.sign_up, name="sign_up"),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,26 +1,42 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
+from .forms import *
 
 def home(request):
-    return render(request, 'usersprofiles/home.html')
+    return render(request, 'profiles/home.html')
 
-def authentication(request): #sign up & login with the same method
+def sign_up(request):
+    # if this is a POST request we need to process the form data
     if request.method == 'POST':
-        # Post data
-        username = request.POST.get('username', None)
-        password = request.POST.get('password', None)
+        # create a form instance and populate it with data from the request
+        user_form = UserForm(request.POST)
+        profile_form = ProfileForm(request.POST, request.FILES)
 
-        #debemos diferenciar si es el form de login o el de sign up usando el atributo
-        #action definido en el HTML form
-        action = request.POST.get('action', None) #valor de action que venga en el POST DE MI REQUEST
+        # check whether it's valid
+        if user_form.is_valid() and profile_form.is_valid():
+            # Create a new instance from the form's data
+            new_user = user_form.save()
+            # Form brings back a plain text string, not an encrypted password
+            pw = new_user.password
+            # Thus we need to use set password to encrypt the password string
+            new_user.set_password(pw)
+            # Save the new instance
+            new_user.save()
 
-        if action == "signup":
-            user = User.objects.create_user(username=username, password=password) #crear un usuario
-            user.save()
-        elif action == "login":
-            user = authenticate(username=username, password=password) #validar que el username y password coincida con algun usuario registrado
-            login(request, user)
-        return redirect('/')
+            # Create, but don't save the new profile instance
+            new_profile = profile_form.save(commit=False)
+            # Set user field
+            new_profile.user = new_user
+            # Save new profile
+            new_profile.save()
 
-    return render(request, 'login.html', {})
+            # redirect to a new URL:
+            return HttpResponseRedirect('/users/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        user_form = UserForm()
+        profile_form = ProfileForm()
+
+    return render(request, 'profiles/sign_up.html', {'user_form': user_form, 'profile_form': profile_form})

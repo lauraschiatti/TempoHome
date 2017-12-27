5cf1bb583838c4d89b7e3029548ac3cac13a1d70
lauricdd <lauri_cdd@hotmail.com>
2017-12-27 11:36:36 +0100

    Room create method to store and ListView to display stored rooms. Templates and urls

--- accommodation/urls.py
+++ accommodation/urls.py
@@ -1,6 +1,17 @@
 from django.conf.urls import url
 from . import views
+from .views import (
+    RoomList
+   # CourseDetail,
+   # CourseCreation,
+   # CourseUpdate,
+   # CourseDelete
+)
+
 
 urlpatterns = [
     url(r'^dashboard$', views.dashboard, name="dashboard"),
+    url(r'^rooms$', RoomList.as_view(), name='rooms_list'),
+    url(r'^rooms/new$', views.create_room, name='new_room'),
+
 ]
--- accommodation/views.py
+++ accommodation/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.views.generic import ListView
+from django.contrib.auth.models import User
+from django.forms import modelformset_factory
 
-# Create your views here.
+from .models import Room, Picture
+from .forms import RoomForm, PictureForm
+
+def dashboard(request):
+    return render(request, 'accommodation/dashboard.html')
+
+# @login_required
+def create_room(request):
+    # formsets allow the user to store several pictures at once
+    PicureFormSet = modelformset_factory(Picture, form=PictureForm)
+
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request
+        room_form = RoomForm(request.POST)
+        picture_formset = PicureFormSet(request.POST, request.FILES, queryset=Picture.objects.none())
+
+        # check whether it's valid
+        if room_form.is_valid() and picture_formset.is_valid():
+            new_room = room_form.save(commit=False)
+            new_room.user = request.user
+            new_room.save()
+
+            for form in picture_formset.cleaned_data:
+                picture = Picture(picture=form['picture'], room=new_room)
+                picture.save()
+
+            return HttpResponseRedirect('accommodation/rooms')
+        else:
+            print room_form.errors, picture_formset.errors
+
+    # return blank forms
+    else:
+        room_form = RoomForm()
+        picture_formset = PicureFormSet(queryset=Picture.objects.none())
+
+    return render(request, 'accommodation/room_form.html', {'room_form': room_form, 'picture_formset': picture_formset})
+
+
+class RoomList(ListView):
+   model = Room

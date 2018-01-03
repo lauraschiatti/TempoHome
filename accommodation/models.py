2d860e37e1a76723388d2ec8c713a3351f270b85
lauricdd <lauri_cdd@hotmail.com>
2018-01-03 11:09:32 +0100

    Remove status from room model. Request model

--- accommodation/models.py
+++ accommodation/models.py
@@ -8,14 +8,19 @@ class Room(models.Model):
     address = models.CharField(max_length=150)
     description = models.CharField(max_length=200)
     rules = models.CharField(max_length=200, blank=True)
-    STATUS_CHOICES = (
-        ('F', 'Free'),
-        ('O', 'Occupied'),
-    )
-    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='F')
     user = models.ForeignKey(User)
-
 
 class Picture(models.Model):
     picture = models.ImageField()
     room = models.ForeignKey(Room)
+
+class Request(models.Model):
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    user = models.ForeignKey(User)
+    room = models.ForeignKey(Room)
+    STATUS_CHOICES = (
+        ('P', 'Pending'),
+        ('A', 'Approved'),
+    )
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='P')

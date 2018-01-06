4638c09ebcdcdb41892f10a19865fbcf32d0bc06
lauricdd <lauri_cdd@hotmail.com>
2018-01-06 18:53:31 +0100

    Refactor get_profile method and guest_profile template

--- profiles/urls.py
+++ profiles/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^$', views.home, name="home"),
     url(r'^signup/$', views.sign_up, name="signup"),
     url(r'^login/$', views.authentication, name="login"),
-    url(r'^logout$', auth_views.logout, {'next_page': '/'}, name="logout")
+    url(r'^logout$', auth_views.logout, {'next_page': '/'}, name="logout"),
+    url(r'^account/profile/(?P<username>\w+)$', views.get_profile, name="get_profile")
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -32,13 +32,14 @@ def sign_up(request):
             # Save new profile
             new_profile.save()
 
+            # login new user
             username = user_form.cleaned_data.get('username')
             raw_password = user_form.cleaned_data.get('password')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
 
             # redirect to a new URL:
-            return HttpResponseRedirect('/accommodation/dashboard')
+            return HttpResponseRedirect('/account/dashboard')
 
     # if a GET (or any other method) we'll create a blank form
     else:
@@ -70,3 +71,51 @@ def authentication(request):
             return render(request, 'profiles/login.html', {'message': message})
 
     return render(request, 'profiles/login.html', {})
+
+def get_profile(request, username):
+    user = User.objects.get(username=username)
+
+    if(request.user == user):
+        return render(request, 'profiles/profile.html')
+    else:
+        return render(request, 'profiles/guest_profile.html', {'user': user})
+
+
+    # if request.method == 'POST':
+    #     user_form = UserForm(request.POST)
+    #     profile_form = ProfileForm(request.POST, request.FILES)
+    #
+    #     # check whether it's valid
+    #     if user_form.is_valid() and profile_form.is_valid():
+    #         # Create a new instance from the form's data
+    #         new_user = user_form.save()
+    #         # Form brings back a plain text string, not an encrypted password
+    #         pw = new_user.password
+    #         # Thus we need to use set password to encrypt the password string
+    #         new_user.set_password(pw)
+    #         # Save the new instance
+    #         new_user.save()
+    #
+    #         # Create, but don't save the new profile instance
+    #         new_profile = profile_form.save(commit=False)
+    #         # Set user field
+    #         new_profile.user = new_user
+    #         # Save new profile
+    #         new_profile.save()
+    #
+    #         # redirect to a new URL:
+    #         return HttpResponseRedirect('/accommodation/dashboard')
+    #
+    # # if a GET (or any other method) we'll create a blank form
+    # else:
+    #     user_form = UserForm()
+    #     profile_form = ProfileForm()
+
+
+#     form = MyForm(request.POST or None, instance=instance)
+#     if form.is_valid():
+#         form.save()
+#         return redirect('next_view')
+#
+#
+# return direct_to_template(request, 'my_template.html', {'form': form})
